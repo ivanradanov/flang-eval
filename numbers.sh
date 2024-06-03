f92d8a0dae1d8460be266438a6252e3ca2422314
Ivan Radanov Ivanov <ivanov2@llnl.gov>
2024-06-03 01:53:30 -0700

    new

--- numbers.sh
+++ numbers.sh
@@ -6,7 +6,7 @@ NRUNS=1
 
 . enable.sh /l/ssd/ivanov2/flang-release/install/
 
-for f in numbers-workdistribute.f90 numbers-omp-traditional.f90 numbers-cpu.f90; do
+for f in matmul/*.f90 synthetic-sqrt/*.f90; do
     echo $f
     flang-new -g -O2 -fopenmp --offload-arch=native -L/l/ssd/ivanov2/flang/install/lib/ $f -o a.out -lFortranOmpRuntime &> /dev/null
     for i in $(seq 1 "$NRUNS"); do
